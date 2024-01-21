dfadf818e48633e5636f05da90fdea50a5fb240d
linnmeso <nmesomachukwu34@gmail.com>
2024-01-21 15:21:53 -0800

    made some corrections and pushing them to git hub

---
+++ settime_webdevelopment101/Java Script/conditonals.js
@@ -0,0 +1,11 @@
+var score = 50;
+
+if (score >= 50) {
+        console.log("Excellent")
+}
+else if (score >= 40){
+        console.log("Good")
+}
+else if (score >= 20) {
+        console.log("fail")
+}
